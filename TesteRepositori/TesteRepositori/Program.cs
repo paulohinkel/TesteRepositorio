1d9647b4955bd48a28fb50761727569f3a5fd7a5
PauloHinkel <paulohinkel@gmail.com>
2015-04-23 13:46:04 -0300

    subindo para o github

---
+++ TesteRepositori/TesteRepositori/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteRepositori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        string Operacao (string ola)
+        {
+            return ola;        
+        }
+
+    }
+}
